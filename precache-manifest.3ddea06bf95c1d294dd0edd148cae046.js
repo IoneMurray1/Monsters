445233123d7d1607f74d1e71c5d09d1e858b86f5
IoneMurray1 <ionemurray1@gmail.com>
2020-04-24 15:38:03 +0100

    Updates

---
+++ precache-manifest.3ddea06bf95c1d294dd0edd148cae046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5323c19ef3a575673a516f5781ba9ea2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5097c0b3a8d949d6b16a",
+    "url": "/monsters/static/css/main.f4663917.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5097c0b3a8d949d6b16a",
+    "url": "/monsters/static/js/main.e652e26c.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
